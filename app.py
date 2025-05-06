e6ebf3257cd688f0b03758e5a968ac8749f3b22f
JulianAtlasovich <134438644+JulianAtlasovich@users.noreply.github.com>
2025-05-06 21:59:07 +0000

    2

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import pandas as pd
-from io import BytesIO
-import zipfile
-import datetime
+#import pandas as pd
+#from io import BytesIO
+#import zipfile
+#import datetime
 
 
 
 if __name__ == "__main__":
-    st.title("Test Site imp")    
+    st.title("Test Site imp 2")    
